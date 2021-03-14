3364b1e275f808fd089849915276742cb7d14d54
Gokul Prathin <gokulprathin8@gmail.com>
2021-03-15 00:04:37 +0530

    added requirements.txt

--- backend/api/notifications/urls.py
+++ backend/api/notifications/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('notifications', views.NotificationViewSet, basename='notifications')
+# router.register('notifications', views.NotificationViewSet, basename='notifications')
 urlpatterns = [
     path(r'', include(router.urls)),
 ]
