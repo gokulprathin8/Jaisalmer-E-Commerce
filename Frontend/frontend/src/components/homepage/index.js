740763eb84c73daeef67fb93538057a05aa60c77
saketh018 <saketh50@gmail.com>
2021-05-10 21:52:09 +0530

    added carousel in landing page

--- Frontend/frontend/src/components/homepage/index.js
+++ Frontend/frontend/src/components/homepage/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.css";
 
 import {
   Navbar,
@@ -7,6 +8,7 @@ import {
   Form,
   FormControl,
   Button,
+  Carousel,
 } from "react-bootstrap";
 
 // import reducers here
@@ -14,36 +16,77 @@ import {
 const Homepage = () => {
   return (
     <React.Fragment>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Jaisalmer E-Commerce</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Account</Nav.Link>
-            <NavDropdown title="Products" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Electronics
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Grocery</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Home Accessories
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Fashion</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.5">Other's</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl
-              type="text"
-              placeholder="Search for products"
-              className="mr-sm-2"
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Jaisalmer E-Commerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Account</Nav.Link>
+              <NavDropdown title="Products" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Electronics
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Grocery</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Home Accessories
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Fashion</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.5">Other's</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search for products"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+      <div className="carousel">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="carousel"
+              src="https://source.unsplash.com/random"
+              alt="First slide"
             />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
+            <Carousel.Caption>
+              <h3>Promotions</h3>
+              <p>view is giving but 1 get 1 free</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="carousel"
+              src="https://source.unsplash.com/random"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Discounts</h3>
+              <p>Get 50% when bill is more than 1000</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="carousel"
+              src="https://source.unsplash.com/random"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Offer's</h3>
+              <p>buy two shirts and get one free</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </React.Fragment>
   );
 };
