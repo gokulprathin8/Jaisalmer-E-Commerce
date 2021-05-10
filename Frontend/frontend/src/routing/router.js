e01b95a23bc57c39c04bd1a90ba342ec815304ad
Restyled.io <commits@restyled.io>
2021-05-10 11:07:46 +0000

    Restyled by prettier

--- Frontend/frontend/src/routing/router.js
+++ Frontend/frontend/src/routing/router.js
@@ -1,11 +1,18 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Homepage from "../components/homepage/index";
 
 export default function Redirect() {
-  return (<Router><div><Switch><Route exact path = "/"><Homepage />
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
           </Route>
-        </Switch></div>
-    </Router>);
+        </Switch>
+      </div>
+    </Router>
+  );
 }
