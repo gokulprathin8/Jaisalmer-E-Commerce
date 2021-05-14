a966d5f5131f3608e9e91388ef986c408c683c77
Restyled.io <commits@restyled.io>
2021-05-14 19:22:22 +0000

    Restyled by autopep8

--- ML/NLP-Reviews/Review.py
+++ ML/NLP-Reviews/Review.py
@@ -1,78 +1,80 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import numpy as np
+import nltk
+import re
 import pandas as pd
 
-#Importing Dataset
-dataset = pd.read_json("Beauty_5.json",lines=True)
+# Importing Dataset
+dataset = pd.read_json("Beauty_5.json", lines=True)
 
-#Droping Columns
-col_drop = ['reviewerID','reviewerName','helpful',
-            'unixReviewTime','reviewTime','summary']
+# Droping Columns
+col_drop = ['reviewerID', 'reviewerName', 'helpful',
+            'unixReviewTime', 'reviewTime', 'summary']
 
-dataset.drop(columns=col_drop,axis=1,inplace=True)
+dataset.drop(columns=col_drop, axis=1, inplace=True)
 
-#Renaming The Column Names
+# Renaming The Column Names
 
-dataset.rename(columns={'asin':'ProductID', 
-                        'reviewText':'Review' ,},inplace=True)
+dataset.rename(columns={'asin': 'ProductID',
+                        'reviewText': 'Review', }, inplace=True)
 
 dataset['Rating'] = dataset['overall']
-dataset.drop(columns=['overall'],axis=1,inplace=True)
+dataset.drop(columns=['overall'], axis=1, inplace=True)
 
-#Droping duplicates items
-dataset.drop_duplicates(subset='ProductID',keep='first',inplace=True)
+# Droping duplicates items
+dataset.drop_duplicates(subset='ProductID', keep='first', inplace=True)
 dataset.reset_index(inplace=True)
-to_drop = ['ProductID','Rating']
+to_drop = ['ProductID', 'Rating']
 
-X = dataset.drop(to_drop,axis=1)
+X = dataset.drop(to_drop, axis=1)
 y = dataset['Rating']
 
-#Text Cleaning
+# Text Cleaning
 
-import re
-import nltk
-import numpy as np
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
 
 corpus = []
 lematizer = WordNetLemmatizer()
 
 sentences = X.copy()
 
-#Getting Clean Reviews
+# Getting Clean Reviews
 
-for i in range(0 , len(sentences)) :
-    review = re.sub('[^a-zA-Z]',' ',sentences['Review'][i])
+for i in range(0, len(sentences)):
+    review = re.sub('[^a-zA-Z]', ' ', sentences['Review'][i])
     review = review.lower()
     review = review.split()
-    review = [lematizer.lemmatize(word)for word in review if word not in set(stopwords.words('english'))]
+    review = [lematizer.lemmatize(word)for word in review if word not in set(
+        stopwords.words('english'))]
     review = ' '.join(review)
     corpus.append(review)
-    
-#Using TF-IDF model
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+# Using TF-IDF model
+
 tfidf = TfidfVectorizer()
 
 X_final = tfidf.fit_transform(corpus).toarray()
 y_final = np.array(y)
 
 
-#Splitting data into training set and test set
+# Splitting data into training set and test set
 
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(X_final,y_final,test_size=0.25,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_final, y_final, test_size=0.25, random_state=42)
 
-#Using RandomForestRegressor
+# Using RandomForestRegressor
 
-from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(random_state=0)
-regressor.fit(X_train,y_train)
+regressor.fit(X_train, y_train)
 
-#Testing the model
+# Testing the model
 y_pred = regressor.predict(X_test)
 
-#Checking the accuracy of the model
+# Checking the accuracy of the model
 
 errors = abs(y_pred - y_test)
 print('Metrics for RandomForestRegressor Trained on Original Data')
