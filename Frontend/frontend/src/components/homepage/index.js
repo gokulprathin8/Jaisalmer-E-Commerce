5f5c6e384d016c0f00cf75d00659afb436abea96
Restyled.io <commits@restyled.io>
2021-05-16 11:41:08 +0000

    Restyled by clang-format

--- Frontend/frontend/src/components/homepage/index.js
+++ Frontend/frontend/src/components/homepage/index.js
@@ -1,19 +1,19 @@
-import React from "react";
 import "./styles.css";
 
+import React from "react";
 import {
-  Navbar,
-  Nav,
-  NavDropdown,
+  Button,
+  Card,
+  Carousel,
+  Col,
+  Container,
   Form,
   FormControl,
-  Button,
-  Carousel,
-  Container,
-  Row,
-  Col,
-  Card,
   Image,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Row,
 } from "react-bootstrap";
 
 // import reducers here
